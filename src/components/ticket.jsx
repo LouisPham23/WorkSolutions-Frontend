17b6160a801221b37be68da3c76460ca09f88d88
Louis Pham <louispham23@gmail.com>
2020-03-20 23:14:40 -0700

    added tickets

---
+++ src/components/ticket.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Ticket = ({
+  Ticket_number,
+  Ticket_type,
+  Priority,
+  Description,
+  Status,
+  Ticket_title
+}) => {
+  return (
+    <div
+      className={`px-4 py-4 mx-4 my-2 rounded-md border-l-4 bg-white shadow ${
+        Status === "Closed"
+          ? "border-green-500"
+          : Status === "Open"
+          ? "border-blue-500"
+          : Status === "Work In Progress"
+          ? "border-yellow-500"
+          : ""
+      }`}
+    >
+      <div className="font-bold text-blue-700">{Ticket_title}</div>
+      <h1 className="font-bold text-gray-700 py-2 text-xl">
+        Ticket Number: {Ticket_number}
+      </h1>
+      <div className="flex text-gray-500">
+        <h1 className="mr-4">Priority: {Priority}</h1>
+        <h1>Status: {Status}</h1>
+      </div>
+      <h2 className="py-2">{Ticket_type}</h2>
+
+      <h1 className="pb-2">Description: {Description}</h1>
+    </div>
+  );
+};
+
+export default Ticket;
--- src/pages/dashboard_page.jsx
+++ src/pages/dashboard_page.jsx
@@ -1,12 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Ticket from "../components/ticket";
 
 const Dashboard_page = () => {
   const [ticket, setTicket] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  let url = "https://csc174proj.herokuapp.com/ticket";
+
+  const [err, setErr] = useState(false);
+
+  const getTickets = async () =>
+    await fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        setTicket(data);
+        setIsLoading(false);
+      })
+      .catch(err => setErr(err));
+  console.log(ticket);
+
+  useEffect(() => {
+    getTickets();
+  }, []);
 
   return (
     <div className="">
-      <h1 className="font-bold px-4 my-8">Dashboard</h1>
-      <div></div>
+      <h1 className="font-bold px-4 mx-1 my-8">Dashboard</h1>
+      {err ? (
+        <h1 className="px-2 rounded mx-4 py-4 bg-red-200 text-red-900">
+          Error loading tickets, please contact server
+        </h1>
+      ) : isLoading ? (
+        <h1 className="px-2 rounded mx-4 py-4 bg-indigo-200 text-indigo-900">
+          Loading tickets...
+        </h1>
+      ) : (
+        <div className="flex flex-col">
+          {ticket.map(ticket => (
+            <Ticket
+              Ticket_type={ticket.Ticket_type}
+              key={ticket.Ticket_number}
+              Ticket_number={ticket.Ticket_number}
+              Priority={ticket.Priority}
+              Description={ticket.Description}
+              Ticket_title={ticket.Ticket_title}
+              Status={ticket.Status}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
